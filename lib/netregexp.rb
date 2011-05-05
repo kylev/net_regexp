19ab1071c29c3741e61f57fba708da5467b5de47
Kyle VanderBeek <kylev@kylev.com>
2011-05-04 21:35:46 -0700

    Upgrade to a more accurate, hand-rolled full octet matcher.

--- lib/netregexp.rb
+++ lib/netregexp.rb
@@ -1,19 +1,21 @@
 require 'ipaddr'
 
 class NetRegexp < Regexp
-  def initialize(ip, cidr=32)
+  FULL_OCTET_RE = '\.(?:\d|1?\d\d|2[0-4]\d|25[0-5])'
+
+  def initialize(ip_str, cidr=32)
     if cidr < 0 || cidr > 32
       raise ArgumentError.new("CIDR must be between 0 and 32 inclusive.")
     end
 
-    @ip = IPAddr.new(ip) # Should force ArgumentError on non-valid IPs
-    @ip_str = ip
+    @ip = IPAddr.new(ip_str) # Should force ArgumentError on non-valid IPs
+    @ip_str = ip_str
     @cidr = cidr
 
     if cidr == 32
-      super "^#{ip.tr('.', '\.')}$"
+      super "^#{ip_str.tr('.', '\.')}$"
     elsif [8, 16, 24].include? cidr
-      super "^#{classful_pattern(cidr / 8)}$"
+      super "^#{NetRegexp.classful_pattern(ip_str, cidr / 8)}$"
     else
       raise "TODO"
     end
@@ -21,8 +23,13 @@ class NetRegexp < Regexp
 
   private
 
-  def classful_pattern(octets)
-    parts = @ip_str.split('.')
-    parts[0...octets].join('\.') + ('\.\d+' * (4 - octets))
+  def self.classful_pattern(ip_str, octets)
+    parts = ip_str.split('.')
+    parts[0...octets].join('\.') + (FULL_OCTET_RE * (4 - octets))
+  end
+
+  def self.range_regexp(low, high)
+    raise ArgumentError.new("Range invalid (#{low}-#{high})") if low > high
+    "[#{low}-#{high}]" # TODO
   end
 end
--- spec/netregexp_spec.rb
+++ spec/netregexp_spec.rb
@@ -5,20 +5,63 @@ describe NetRegexp do
     it "should present an exact match for /32" do
       NetRegexp.new('191.168.4.2').source.should == "^191\.168\.4\.2$"
     end
-    
+
     it "should raise an error on bad arguments" do
-      lambda {NetRegexp.new("555.555.555.555")}.should raise_exception(ArgumentError)
-      lambda {NetRegexp.new("10.0.0.1", -1)}.should raise_exception(ArgumentError)
-      lambda {NetRegexp.new("32.0.0.1", 33)}.should raise_exception(ArgumentError)
+      lambda { NetRegexp.new("555.555.555.555") }.should raise_exception(ArgumentError)
+      lambda { NetRegexp.new("10.0.0.1", -1) }.should raise_exception(ArgumentError)
+      lambda { NetRegexp.new("32.0.0.1", 33) }.should raise_exception(ArgumentError)
+    end
+  end
+
+  describe "FULL_OCTET_RE" do
+    before(:each) do
+      # Test exact match only because it'll be pinned that way inside a larger regexp
+      @octet = Regexp.new("^#{NetRegexp::FULL_OCTET_RE}$")
+    end
+
+    it "should match .0-255" do
+      (0..255).each do |i|
+        ".#{i}".should match @octet
+      end
+    end
+
+    it "should not match a bare number" do
+      '1'.should_not match @octet
+    end
+
+    it "should not match out of bounds values" do
+      '.256'.should_not match @octet
+      '.2550'.should_not match @octet
     end
   end
 
   describe "classful matches" do
     it "should handle /8" do
       re = NetRegexp.new('10.0.0.0', 8)
-      puts re
       "10.10.32.43".should match re
       '110.10.42.23'.should_not match re
+    end
+  end
+
+  describe "#range_regexp" do
+    it "should raise on an invalid range" do
+      lambda { NetRegexp.range_regexp(42, 24) }.should raise_exception(ArgumentError)
+      lambda { NetRegexp.range_regexp(33, 33) }.should_not raise_exception(ArgumentError)
+    end
+
+    it "should handle single digit ranges" do
+      re = Regexp.new("^#{NetRegexp.range_regexp(2, 7)}$")
+      '1'.should_not match re
+      '2'.should match re
+      '5'.should match re
+      '7'.should match re
+      '8'.should_not match re
+      '25'.should_not match re
+    end
+
+    it "should handle single to double digit ranges" do
+      # re = Regexp.new("^#{NetRegexp.range_regexp(4, 21)}$")
+      # '1'.should_not match re
     end
   end
 end
